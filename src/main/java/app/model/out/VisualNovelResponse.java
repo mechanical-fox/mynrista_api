15015dc99ee1a42739c66cb4a5727ad4213adb60
Meunier Pierre <meunier.pierre108@gmail.com>
2026-02-18 11:04:29 +0100

    :art: suppression import inutilisé

--- src/main/java/app/model/out/VisualNovelResponse.java
+++ src/main/java/app/model/out/VisualNovelResponse.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.sql.Date;
 
 import app.model.database.VisualNovelEntity;
 import app.util.Util;
